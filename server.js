1cfdf9ed4f65e881caf19aad91827b64cdb8b037
Pradnyesh <pradh.panch@gmail.com>
2026-03-19 13:16:28 +0530

    add download api

--- server.js
+++ server.js
@@ -7,9 +7,23 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
+app.get("/download", (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.send("Please provide video URL");
+  }
+
+  res.json({
+    message: "Processing video...",
+    video_url: url,
+    download_link: "https://example.com/video.mp4"
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
