0332b2c4a15ebb239baed1b88a24c19584196160
Pradnyesh <pradh.panch@gmail.com>
2026-03-19 13:08:48 +0530

    fix port issue

--- server.js
+++ server.js
@@ -2,20 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
-app.use(express.json());
 
-app.post("/process", (req, res) => {
-  const url = req.body.url;
-
-  res.json({
-    status: "success",
-    message: "API working 🔥",
-    link_received: url
-  });
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
